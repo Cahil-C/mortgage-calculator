dcbb1a430c93f1b612c05a451147e9d1b8bd4c64
Cahil Cowan <kevinc20000720@gmail.com>
2026-03-10 09:51:48 -0500

    Add DOM selectors

---
+++ script/script.js
@@ -0,0 +1,17 @@
+// SELECTORS
+const mortgageForm = document.getElementById('mortgageForm');
+const clearBtn = document.getElementById('clearBtn');
+const amountInput = document.getElementById('amount');
+const termInput = document.getElementById('term');
+const rateInput = document.getElementById('rate');
+const calculateBtn = document.getElementById('calculateBtn');
+const resultsDefault = document.getElementById('resultsDefault');
+const resultsState = document.getElementById('resultsState');
+const monthlyResult = document.getElementById('monthlyResult');
+const totalResult = document.getElementById('totalResult');
+
+// ERRORS
+const amountError = document.getElementById('amountError');
+const termError = document.getElementById('termError');
+const rateError = document.getElementById('rateError');
+const typeError = document.getElementById('typeError');
